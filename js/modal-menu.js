e725c800c6e87f4213a6b6f9f84a3cd28a745c58
e.a.kuzhilskiy <e.a.kuzhilskiy@gmail.com>
2023-04-26 23:36:35 +0300

    hw7-new

--- js/modal-menu.js
+++ js/modal-menu.js
@@ -1,15 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[js-open-menu]"),
-      closeModalBtn: document.querySelector("[js-close-menu]"),
-      modal: document.querySelector("[js-menu-container]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-open");
-    }
-  })();
-  
+  const refs = {
+    openModalBtn: document.querySelector("[js-open-menu]"),
+    closeModalBtn: document.querySelector("[js-close-menu]"),
+    modal: document.querySelector("[js-menu-container]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
